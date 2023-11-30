1566d7bf69bb08f9c681a447340c1e9561336873
NekoSans <2341257-NekoSans@users.noreply.replit.com>
2023-11-30 01:09:45 +0000

    Added node functionality
    - Added points to adjacent nodes
    - Added constructor to take vector of nodes or to take all points to nodes separately
    - resetVector() -> when making chnages to adjacent points modify the vector at the correct index and then call resetVector to modify the pointer
    - inheritNeighbors() -> to make inserting a node simpler call this with the node you want to remove which will make all of the connections necessary and return new node
    - getNumberOfNeighbors() -> returns the number of living neighbors

--- gameClasses/Node.h
+++ gameClasses/Node.h
@@ -1,44 +1,131 @@
-
-
-
 #include<vector>
+#include<string>
+#include<stdexcept>
+
 #include "GameBoard.h"
 #ifndef NODE_H
 #define NODE_H
 
+//Throws runtime error when you pass vector into Node(vector) constructor
+#define CHECK_EMPTY_VECTOR(vector) \
+if (!vector.empty()) { \
+  throw std::runtime_error("Vector passed to Node constructor is empty"); \
+}
+
+
 //inheriting from gameboard
-class Node : GameBoard {
+class Node {
   private:
     bool isAlive;
     int* xPos;
     int* yPos;
-    int* daysAlive();
-    std::vector<Node*> neighbors;
+    int* daysAlive;
+    std::vector<Node*> neighbors; //clockwise 0-top, (size - 1) - top left
+
+    //All neighbors
+    Node* top;
+    Node* topR;
+    Node* right;
+    Node* bottomR;
+    Node* bottom;
+    Node* bottomL;
+    Node* left;
+    Node* topL;
 
     //TODO: Add clust obj representing the group of nodes that this node is part of
 
   public:
-    //getters
+//-------------------------------------------NODE Getters--------------------------------------
     bool isDead() const;
     int getXPos() const;
     int getYPos() const;
-    int getDaysAlive() const;
+    int getDaysAlive();
     std::vector<Node*> getNeighbors();
+    void setNeighbor(Node* newN, int location); //Location represents 0 to 8 where 0 is top moving clockwise and 8 - top left
+    int getNumberOfNeighbors();
 
-    //setters
+    //Node getters
+    Node* getTop();
+    Node* getTopR();
+    Node* getRight();
+    Node* getBottomR();
+    Node* getBottom();
+    Node* getBottomL();
+    Node* getLeft();
+    Node* getTopL();
+
+    //-------------------------------------------NODE SETTERS--------------------------------------
     void kill();
     void revive();
     void setPos(int x, int y);
-    void addNeighbor(Node* newN);
 
-    //Test functions
-    void printNeigborsTest();
+    void setTop(Node* newN);
+    void setTopR(Node* newN);
+    void setRight(Node* newN);
+    void setBottomR(Node* newN);
+    void setBottom(Node* newN);
+    void setBottomL(Node* newN);
+    void setLeft(Node* newN);
+    void setTopL(Node* newN);
+
+    void setByIndex(int index, Node* toSet);
 
     //Constructor
-    Node(bool alive = false, std::vector<Node*> neighbors) {
+    Node(Node* top = nullptr,
+         Node* topR = nullptr,
+         Node* right = nullptr,
+         Node* bottomR = nullptr,
+         Node* bottom = nullptr,
+         Node* bottomL = nullptr,
+         Node* left = nullptr,
+         Node* topL = nullptr, bool alive = false,) {
+      
+      //Setting all nodes to given nodes
+      this->top = top;
+      this->neighbors.push_back(this->top);
+      this->topR = topR;
+      this->neighbors.push_back(this->topR);
+      this->right = right;
+      this->neighbors.push_back(this->right);
+      this->bottomR = bottomR;
+      this->neighbors.push_back(this->bottomR);
+      this->bottom = bottom;
+      this->neighbors.push_back(this->bottom);
+      this->bottomL = bottomL;
+      this->neighbors.push_back(this->bottomL);
+      this->left = left;
+      this->neighbors.push_back(this->left);
+      this->topL = topL;
+      this->neighbors.push_back(this->topL);
+
+      this->isAlive = false;
+      *(this->daysAlive) = 0;
+      }
+
+    Node(bool alive, std::vector<Node*> neighbors) {
+      //Throws system error if we send an empty list here
+      CHECK_EMPTY_VECTOR(neighbors);
+      this->top = neighbors.at(0);
+      this->topR = neighbors.at(1);
+      this->right = neighbors.at(2);
+      this->bottomR = neighbors.at(3);
+      this->bottom = neighbors.at(4);
+      this->bottomL = neighbors.at(5);
+      this->left = neighbors.at(6);
+      this->topL = neighbors.at(7);
+
       this->isAlive = alive;
-      this->neighbors = neighbors;
+      *(this->daysAlive) = 0;
     }
+
+//-------------------------------------------UTILITY--------------------------------------
+/* Function to make removing and adding mutated nodes easier. all old neighbors go to new nodes neighbors and returns a dead node with the
+   same neighbors and delete oldN
+*/
+Node* inheritNeighbors(Node* oldN);
+void resetVector();
+//-------------------------------------------OVERLOADED OPERATORS--------------------------------------
+
 };
 
 #endif
--- gameClasses/node.cpp
+++ gameClasses/node.cpp
@@ -2,41 +2,84 @@
 #include "Node.h"
 using namespace std;
 
+//-------------------------------------------NODE GETTERS--------------------------------------
 //simple getters - const for later assighnment overload (dont know if it will be necessary)
-bool Node::isDead() const {return *isAlive;}
+bool Node::isDead() const {return isAlive;}
 int Node::getXPos() const {return *xPos;}
 int Node::getYPos() const {return *yPos;}
-int Node::getDaysAlive() const {return *daysAlive;}
-vector<Node*> Node::getNeighbors() const {return neighbors;}
+int Node::getDaysAlive() {return *daysAlive;}
+vector<Node*> Node::getNeighbors() {return neighbors;}
 
-//Simple setters
+//Getters for nodes
+Node* Node::getTop() {return this->top;}
+Node* Node::getTopR() {return this->topR;}
+Node* Node::getRight() {return this->right;}
+Node* Node::getBottomR() {return this->bottomR;}
+Node* Node::getBottom() {return this->bottom;}
+Node* Node::getBottomL() {return this->bottomL;}
+Node* Node::getLeft() {return this->left;}
+Node* Node::getTopL() {return this->topL;}
+
+//Returns number of living neighbors to be used in process
+int Node::getNumberOfNeighbors() {
+  int counter;
+  for (int i = 0; i < neighbors.size(); i++) {
+    if (neighbors.at(i)->isDead() == false) {counter++;};
+  }
+  return counter;
+}
+
+//-------------------------------------------NODE SETTERS--------------------------------------
+void Node::setTop(Node* newN) {this->top = newN;}
+void Node::setTopR(Node* newN) {this->topR = newN;}
+void Node::setRight(Node* newN) {this->right = newN;}
+void Node::setBottomR(Node* newN) {this->bottomR = newN;}
+void Node::setBottom(Node* newN) {this->bottom = newN;}
+void Node::setBottomL(Node* newN) {this->bottomL = newN;}
+void Node::setLeft(Node* newN) {this->left = newN;}
+void Node::setTopL(Node* newN) {this->topL = newN;}
+
+void Node::setByIndex(int index, Node* toSet) {
+  this->neighbors.at(index) = toSet;
+}
+
 void Node::kill() {isAlive = false;}
 void Node::revive() {isAlive = true;}
 void Node::setPos(int x, int y) {
-  //TODO: Out of game bounds check
   *xPos = x;
   *yPos = y;
 }
-//TODO: Modify this so that the newN is put in the correct empty index of the neigbors vector
-/* i.e.
-Node -> neigbors {topL = null, top, topR, right = null, botomR, bottom, bottomL = null, left = null}
-Node to Insert -> NewN, left
-Node -> neigbors {topL = null, top, topR, right = null, botomR, bottom, bottomL = null, left = newN}
-*/
-void Node::addNeighbor(Node* newN, int location) {
-  neighbors.add(newN);
+
+//-------------------------------------------UTILITY--------------------------------------
+void Node::resetVector() {
+  this->top = neighbors.at(0);
+  this->topR = neighbors.at(1);
+  this->right = neighbors.at(2);
+  this->bottomR = neighbors.at(3);
+  this->bottom = neighbors.at(4);
+  this->bottomL = neighbors.at(5);
+  this->left = neighbors.at(6);
+  this->topL = neighbors.at(7);
 }
 
-//Test function to pring locations of neighbors
-void Node::printNeigborsTest(Node* parent) {
-  vector::const_iterator itr = parent->neighbors.begin();
-  while (itr != parent->neighbors.end()) {
-    cout << "Neighbors at X: " << *xPos << ", Y: " << *yPos << endl;
-    itr++;
+//Will inherit a target nodes links allowing for easier node insertion and deletion
+Node* Node::inheritNeighbors(Node* old) {
+  //New Node will point to it's neighbors
+  Node* newN = new Node(false, old->getNeighbors());
+
+  //Now to set node neighbors to point to newN instead of oldN
+  for (int i = 0; i < 8; i++) {
+    //Search through all 8 neighbors
+    for (int j = 0; j < 8; j++) {
+      Node* temp = newN->getNeighbors().at(i)->getNeighbors().at(j);
+      //Search through adjacent neighbors neighbors to find the pointer pointing to old
+      if (&temp == &old) {
+        //make correct change in neighbors vectors
+        temp->getNeighbors().at(j) = newN;
+        //force reset vector to change target node
+        temp->resetVector();
+      }
+    }
   }
-}
-
-//TODO: check conditions from game rules on time step
-void 
-
+  
 }
