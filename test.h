40e1d6caa20add1afc353bee1bcd84d67ef17142
ka-reem <71798166+ka-reem@users.noreply.github.com>
2023-12-03 11:02:08 +0000

    test_h

---
+++ test.h
@@ -0,0 +1,12 @@
+#ifndef TEST_H 
+#define TEST_H 
+
+void testGameBoardInitialization();
+void testPrinting();
+void testStillLifes1();
+void testStillLifes2();
+void testStillLifes3();
+void testStillLifes4();
+void testStillLifes5();
+
+#endif // TEST_H 
